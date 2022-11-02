54a3a29745cddd8ee36473b95fbf8ee131173add
Marko Veniger <marko.veniger@htecgroup.com>
2022-11-02 14:22:00 +0100

    local work, rename and fix functions

--- tests/catch/unit/texture/hipTexObjectTests.cc
+++ tests/catch/unit/texture/hipTexObjectTests.cc
@@ -21,11 +21,11 @@ THE SOFTWARE.
 
 
 class TexObjectTestWrapper {
-private:
-  float *mHostData;
+ private:
+  float* mHostData;
   bool mOmmitDestroy;
-  
-public:
+
+ public:
   hipTextureObject_t mTextureObject = 0;
   HIP_RESOURCE_DESC mResDesc;
   HIP_TEXTURE_DESC mTexDesc;
@@ -33,26 +33,26 @@ public:
   HIP_ARRAY_DESCRIPTOR mArrayDesc;
   hiparray mArray;
   size_t mSize; /* size in bytes*/
-  int mWidth; /* width in elements */
+  int mWidth;   /* width in elements */
 
-  TexObjectTestWrapper(bool useResourceViewDescriptor, bool ommitDestroy = false): mOmmitDestroy(ommitDestroy), mWidth(128) {
-
-    int i;    
+  TexObjectTestWrapper(bool useResourceViewDescriptor, bool ommitDestroy = false)
+      : mOmmitDestroy(ommitDestroy), mWidth(128) {
+    int i;
     mSize = mWidth * sizeof(float);
-  
-    mHostData = (float *) malloc(mSize);
+
+    mHostData = (float*)malloc(mSize);
     memset(mHostData, 0, mSize);
-  
-    for(i = 0; i < mWidth; i++) {
+
+    for (i = 0; i < mWidth; i++) {
       mHostData[i] = i;
     }
-    
+
     memset(&mArrayDesc, 0, sizeof(mArrayDesc));
     mArrayDesc.Format = HIP_AD_FORMAT_FLOAT;
     mArrayDesc.NumChannels = 1;
     mArrayDesc.Width = mWidth;
     mArrayDesc.Height = 0;
-    
+
     HIP_CHECK(hipArrayCreate(&mArray, &mArrayDesc));
     HIP_CHECK(hipMemcpyHtoA(reinterpret_cast<hipArray*>(mArray), 0, mHostData, mSize));
 
@@ -60,26 +60,26 @@ public:
     mResDesc.resType = HIP_RESOURCE_TYPE_ARRAY;
     mResDesc.res.array.hArray = mArray;
     mResDesc.flags = 0;
-  
+
     memset(&mTexDesc, 0, sizeof(mTexDesc));
     mTexDesc.filterMode = HIP_TR_FILTER_MODE_POINT;
     mTexDesc.flags = 0;
-    
+
     memset(&mResViewDesc, 0, sizeof(mResViewDesc));
-    
+
 #if HT_AMD
-    if(useResourceViewDescriptor) {
+    if (useResourceViewDescriptor) {
       mResViewDesc.format = HIP_RES_VIEW_FORMAT_FLOAT_1X32;
       mResViewDesc.width = mSize;
     }
 #endif
-  
-    HIP_CHECK(hipTexObjectCreate(&mTextureObject, &mResDesc, &mTexDesc, useResourceViewDescriptor ? &mResViewDesc : nullptr));
-    
+
+    HIP_CHECK(hipTexObjectCreate(&mTextureObject, &mResDesc, &mTexDesc,
+                                 useResourceViewDescriptor ? &mResViewDesc : nullptr));
   }
-  
+
   ~TexObjectTestWrapper() {
-    if(!mOmmitDestroy) {
+    if (!mOmmitDestroy) {
       HIP_CHECK(hipTexObjectDestroy(mTextureObject));
     }
     HIP_CHECK(hipArrayDestroy(mArray));
@@ -87,121 +87,118 @@ public:
   }
 };
 
-/* hipGetTexObjectResourceDesc tests */
+/* hipTexObjectGetResourceDesc tests */
 
 TEST_CASE("Unit_hipGetTexObjectResourceDesc_positive") {
   CHECK_IMAGE_SUPPORT
-  
-  #if HT_AMD
-  
+
+#if HT_AMD
+
   TexObjectTestWrapper texObjWrapper(false);
-  
+
   HIP_RESOURCE_DESC checkDesc;
   memset(&checkDesc, 0, sizeof(checkDesc));
-  
-  HIP_CHECK(hipGetTexObjectResourceDesc(&checkDesc, texObjWrapper.mTextureObject));
-  
+
+  HIP_CHECK(hipTexObjectGetResourceDesc(&checkDesc, texObjWrapper.mTextureObject));
+
   REQUIRE(checkDesc.resType == texObjWrapper.mResDesc.resType);
   REQUIRE(checkDesc.res.array.hArray == texObjWrapper.mResDesc.res.array.hArray);
 
 #else
   HipTest::HIP_SKIP_TEST("Skipping on NVIDIA platform");
 #endif
-
 }
 
 
 TEST_CASE("Unit_hipGetTexObjectResourceDesc_negative") {
   CHECK_IMAGE_SUPPORT
 
-#if HT_AMD  
+#if HT_AMD
 
   TexObjectTestWrapper texObjWrapper(false);
-  
+
   HIP_RESOURCE_DESC checkDesc;
   memset(&checkDesc, 0, sizeof(checkDesc));
-  
+
   SECTION("desc is nullptr") {
-    HIP_CHECK_ERROR(hipGetTexObjectResourceDesc(nullptr, texObjWrapper.mTextureObject), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipTexObjectGetResourceDesc(nullptr, texObjWrapper.mTextureObject),
+                    hipErrorInvalidValue);
   }
-  
+
   SECTION("texture is invalid") {
-    HIP_CHECK_ERROR(hipGetTexObjectResourceDesc(&checkDesc, static_cast<hipTextureObject_t>(0)), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipTexObjectGetResourceDesc(&checkDesc, static_cast<hipTextureObject_t>(0)),
+                    hipErrorInvalidValue);
   }
-  
+
 #else
   HipTest::HIP_SKIP_TEST("Skipping on NVIDIA platform");
 #endif
-
 }
 
-/* hipGetTexObjectResourceViewDesc tests */
 
+/* hipTexObjectGetResourceViewDesc tests */
 
 TEST_CASE("Unit_hipGetTexObjectResourceViewDesc_positive") {
   CHECK_IMAGE_SUPPORT
 #if HT_AMD
   TexObjectTestWrapper texObjWrapper(true);
-  
+
   HIP_RESOURCE_VIEW_DESC checkDesc;
   memset(&checkDesc, 0, sizeof(checkDesc));
-  
-  HIP_CHECK(hipGetTexObjectResourceViewDesc(&checkDesc, texObjWrapper.mTextureObject));
-  
+
+  HIP_CHECK(hipTexObjectGetResourceViewDesc(&checkDesc, texObjWrapper.mTextureObject));
+
   REQUIRE(checkDesc.format == texObjWrapper.mResViewDesc.format);
   REQUIRE(checkDesc.width == texObjWrapper.mResViewDesc.width);
-  
+
 #else
   HipTest::HIP_SKIP_TEST("Skipping on NVIDIA platform");
 #endif
-  
 }
 
 
 TEST_CASE("Unit_hipGetTexObjectResourceViewDesc_negative") {
   CHECK_IMAGE_SUPPORT
-#if HT_AMD 
+#if HT_AMD
   TexObjectTestWrapper texObjWrapper(true);
-  
+
   HIP_RESOURCE_VIEW_DESC checkDesc;
   memset(&checkDesc, 0, sizeof(checkDesc));
-  
+
   SECTION("desc is nullptr") {
-    HIP_CHECK_ERROR(hipGetTexObjectResourceViewDesc(nullptr, texObjWrapper.mTextureObject), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipTexObjectGetResourceViewDesc(nullptr, texObjWrapper.mTextureObject),
+                    hipErrorInvalidValue);
   }
-  
+
   SECTION("texture is invalid") {
-    HIP_CHECK_ERROR(hipGetTexObjectResourceViewDesc(&checkDesc, static_cast<hipTextureObject_t>(0)), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipTexObjectGetResourceViewDesc(&checkDesc, static_cast<hipTextureObject_t>(0)),
+                    hipErrorInvalidValue);
   }
-  
+
 #else
   HipTest::HIP_SKIP_TEST("Skipping on NVIDIA platform");
 #endif
-
 }
 
-
-
-/* hipGetTexObjectTextureDesc tests */
+/* hipTexObjectGetTextureDesc tests */
 
 
 TEST_CASE("Unit_hipGetTexObjectTextureDesc_positive") {
   CHECK_IMAGE_SUPPORT
 #if HT_AMD
   TexObjectTestWrapper texObjWrapper(false);
-  
+
   HIP_TEXTURE_DESC checkDesc;
   memset(&checkDesc, 0, sizeof(checkDesc));
-  
-  HIP_CHECK(hipGetTexObjectTextureDesc(&checkDesc, texObjWrapper.mTextureObject));
-  
+
+  HIP_CHECK(hipTexObjectGetTextureDesc(&checkDesc, texObjWrapper.mTextureObject));
+
   REQUIRE(checkDesc.filterMode == texObjWrapper.mTexDesc.filterMode);
   REQUIRE(checkDesc.flags == texObjWrapper.mTexDesc.flags);
-  
+
 #else
   HipTest::HIP_SKIP_TEST("Skipping on NVIDIA platform");
 #endif
-
 }
 
 
@@ -209,28 +206,30 @@ TEST_CASE("Unit_hipGetTexObjectTextureDesc_negative") {
   CHECK_IMAGE_SUPPORT
 #if HT_AMD
   TexObjectTestWrapper texObjWrapper(false);
-  
+
   HIP_TEXTURE_DESC checkDesc;
   memset(&checkDesc, 0, sizeof(checkDesc));
-  
+
   SECTION("desc is nullptr") {
-    HIP_CHECK_ERROR(hipGetTexObjectTextureDesc(nullptr, texObjWrapper.mTextureObject), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipTexObjectGetTextureDesc(nullptr, texObjWrapper.mTextureObject),
+                    hipErrorInvalidValue);
   }
-  
+
   SECTION("texture is invalid") {
-    HIP_CHECK_ERROR(hipGetTexObjectTextureDesc(&checkDesc, static_cast<hipTextureObject_t>(0)), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipTexObjectGetTextureDesc(&checkDesc, static_cast<hipTextureObject_t>(0)),
+                    hipErrorInvalidValue);
   }
-  
+
 #else
   HipTest::HIP_SKIP_TEST("Skipping on NVIDIA platform");
 #endif
-  
 }
+
+/* hipTexObjectDestroy test */
 
 TEST_CASE("Unit_hipTexObjectDestroy_positive") {
   CHECK_IMAGE_SUPPORT
 
   TexObjectTestWrapper texObjWrapper(false, true);
   REQUIRE(hipTexObjectDestroy(texObjWrapper.mTextureObject) == hipSuccess);
-
 }
--- tests/catch/unit/texture/hipTextureObjectTests.cc
+++ tests/catch/unit/texture/hipTextureObjectTests.cc
@@ -21,60 +21,61 @@ THE SOFTWARE.
 
 
 class TextureObjectTestWrapper {
-private:
-  float *mHostData;
+ private:
+  float* mHostData;
   bool mOmmitDestroy;
-  
-public:
+
+ public:
   hipTextureObject_t mTextureObject = 0;
   hipResourceDesc mResDesc;
   hipTextureDesc mTexDesc;
   hipChannelFormatDesc mChannelDesc;
   hipResourceViewDesc mResViewDesc;
-  hipArray *mArray;
+  hipArray* mArray;
   size_t mSize; /* size in bytes*/
-  int mWidth; /* width in elements */
+  int mWidth;   /* width in elements */
 
-  TextureObjectTestWrapper(bool useResourceViewDescriptor, bool ommitDestroy = false): mOmmitDestroy(ommitDestroy), mWidth(128) {
-
-    int i;    
+  TextureObjectTestWrapper(bool useResourceViewDescriptor, bool ommitDestroy = false)
+      : mOmmitDestroy(ommitDestroy), mWidth(128) {
+    int i;
     mSize = mWidth * sizeof(float);
-  
-    mHostData = (float *) malloc(mSize);
+
+    mHostData = (float*)malloc(mSize);
     memset(mHostData, 0, mSize);
-  
-    for(i = 0; i < mWidth; i++) {
+
+    for (i = 0; i < mWidth; i++) {
       mHostData[i] = i;
     }
-  
+
     mChannelDesc = hipCreateChannelDesc(32, 0, 0, 0, hipChannelFormatKindFloat);
     hipMallocArray(&mArray, &mChannelDesc, mWidth);
-  
+
     HIP_CHECK(hipMemcpy2DToArray(mArray, 0, 0, mHostData, mSize, mSize, 1, hipMemcpyHostToDevice));
 
     memset(&mResDesc, 0, sizeof(mResDesc));
     mResDesc.resType = hipResourceTypeArray;
     mResDesc.res.array.array = mArray;
-  
+
     memset(&mTexDesc, 0, sizeof(mTexDesc));
     mTexDesc.addressMode[0] = hipAddressModeClamp;
     mTexDesc.filterMode = hipFilterModePoint;
     mTexDesc.readMode = hipReadModeElementType;
     mTexDesc.normalizedCoords = false;
-    
+
     memset(&mResViewDesc, 0, sizeof(mResViewDesc));
 #if HT_AMD
-    if(useResourceViewDescriptor) {
+    if (useResourceViewDescriptor) {
       mResViewDesc.format = hipResViewFormatFloat1;
       mResViewDesc.width = mSize;
     }
 #endif
-  
-    HIP_CHECK(hipCreateTextureObject(&mTextureObject, &mResDesc, &mTexDesc, useResourceViewDescriptor ? &mResViewDesc : nullptr));
+
+    HIP_CHECK(hipCreateTextureObject(&mTextureObject, &mResDesc, &mTexDesc,
+                                     useResourceViewDescriptor ? &mResViewDesc : nullptr));
   }
-  
-  ~TextureObjectTestWrapper(){
-    if(!mOmmitDestroy) {
+
+  ~TextureObjectTestWrapper() {
+    if (!mOmmitDestroy) {
       HIP_CHECK(hipDestroyTextureObject(mTextureObject));
     }
     HIP_CHECK(hipFreeArray(mArray));
@@ -86,36 +87,36 @@ public:
 
 TEST_CASE("Unit_hipGetTextureObjectResourceDesc_positive") {
   CHECK_IMAGE_SUPPORT
-  
+
   TextureObjectTestWrapper texObjWrapper(false);
-  
+
   hipResourceDesc checkDesc;
   memset(&checkDesc, 0, sizeof(checkDesc));
-  
+
   HIP_CHECK(hipGetTextureObjectResourceDesc(&checkDesc, texObjWrapper.mTextureObject));
-  
+
   REQUIRE(checkDesc.resType == texObjWrapper.mResDesc.resType);
   REQUIRE(checkDesc.res.array.array == texObjWrapper.mResDesc.res.array.array);
-  
 }
 
 
 TEST_CASE("Unit_hipGetTextureObjectResourceDesc_negative") {
   CHECK_IMAGE_SUPPORT
-  
+
   TextureObjectTestWrapper texObjWrapper(false);
-  
+
   hipResourceDesc checkDesc;
   memset(&checkDesc, 0, sizeof(checkDesc));
-  
+
   SECTION("desc is nullptr") {
-    HIP_CHECK_ERROR(hipGetTextureObjectResourceDesc(nullptr, texObjWrapper.mTextureObject), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipGetTextureObjectResourceDesc(nullptr, texObjWrapper.mTextureObject),
+                    hipErrorInvalidValue);
   }
-  
+
   SECTION("texture is invalid") {
-    HIP_CHECK_ERROR(hipGetTextureObjectResourceDesc(&checkDesc, static_cast<hipTextureObject_t>(0)), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipGetTextureObjectResourceDesc(&checkDesc, static_cast<hipTextureObject_t>(0)),
+                    hipErrorInvalidValue);
   }
-  
 }
 
 /* hipGetTextureObjectResourceViewDesc tests */
@@ -125,44 +126,44 @@ TEST_CASE("Unit_hipGetTextureObjectResourceViewDesc_positive") {
   CHECK_IMAGE_SUPPORT
 #if HT_AMD
   TextureObjectTestWrapper texObjWrapper(true);
-  
+
   hipResourceViewDesc checkDesc;
   memset(&checkDesc, 0, sizeof(checkDesc));
-  
+
   HIP_CHECK(hipGetTextureObjectResourceViewDesc(&checkDesc, texObjWrapper.mTextureObject));
-  
+
   REQUIRE(checkDesc.format == texObjWrapper.mResViewDesc.format);
   REQUIRE(checkDesc.width == texObjWrapper.mResViewDesc.width);
-  
+
 #else
   HipTest::HIP_SKIP_TEST("Skipping on NVIDIA platform");
 #endif
-  
 }
 
 
 TEST_CASE("Unit_hipGetTextureObjectResourceViewDesc_negative") {
   CHECK_IMAGE_SUPPORT
-#if HT_AMD 
+#if HT_AMD
   TextureObjectTestWrapper texObjWrapper(true);
-  
+
   hipResourceViewDesc checkDesc;
   memset(&checkDesc, 0, sizeof(checkDesc));
-  
+
   SECTION("desc is nullptr") {
-    HIP_CHECK_ERROR(hipGetTextureObjectResourceViewDesc(nullptr, texObjWrapper.mTextureObject), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipGetTextureObjectResourceViewDesc(nullptr, texObjWrapper.mTextureObject),
+                    hipErrorInvalidValue);
   }
-  
+
   SECTION("texture is invalid") {
-    HIP_CHECK_ERROR(hipGetTextureObjectResourceViewDesc(&checkDesc, static_cast<hipTextureObject_t>(0)), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(
+        hipGetTextureObjectResourceViewDesc(&checkDesc, static_cast<hipTextureObject_t>(0)),
+        hipErrorInvalidValue);
   }
-  
+
 #else
   HipTest::HIP_SKIP_TEST("Skipping on NVIDIA platform");
 #endif
-
 }
-
 
 
 /* hipGetTextureObjectTextureDesc tests */
@@ -172,21 +173,20 @@ TEST_CASE("Unit_hipGetTextureObjectTextureDesc_positive") {
   CHECK_IMAGE_SUPPORT
 #if HT_AMD
   TextureObjectTestWrapper texObjWrapper(false);
-  
+
   hipTextureDesc checkDesc;
   memset(&checkDesc, 0, sizeof(checkDesc));
-  
+
   HIP_CHECK(hipGetTextureObjectTextureDesc(&checkDesc, texObjWrapper.mTextureObject));
-  
+
   REQUIRE(checkDesc.addressMode[0] == texObjWrapper.mTexDesc.addressMode[0]);
   REQUIRE(checkDesc.filterMode == texObjWrapper.mTexDesc.filterMode);
   REQUIRE(checkDesc.readMode == texObjWrapper.mTexDesc.readMode);
   REQUIRE(checkDesc.normalizedCoords == texObjWrapper.mTexDesc.normalizedCoords);
-  
+
 #else
   HipTest::HIP_SKIP_TEST("Skipping on NVIDIA platform");
 #endif
-
 }
 
 
@@ -194,28 +194,30 @@ TEST_CASE("Unit_hipGetTextureObjectTextureDesc_negative") {
   CHECK_IMAGE_SUPPORT
 #if HT_AMD
   TextureObjectTestWrapper texObjWrapper(false);
-  
+
   hipTextureDesc checkDesc;
   memset(&checkDesc, 0, sizeof(checkDesc));
-  
+
   SECTION("desc is nullptr") {
-    HIP_CHECK_ERROR(hipGetTextureObjectTextureDesc(nullptr, texObjWrapper.mTextureObject), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipGetTextureObjectTextureDesc(nullptr, texObjWrapper.mTextureObject),
+                    hipErrorInvalidValue);
   }
-  
+
   SECTION("texture is invalid") {
-    HIP_CHECK_ERROR(hipGetTextureObjectTextureDesc(&checkDesc, static_cast<hipTextureObject_t>(0)), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipGetTextureObjectTextureDesc(&checkDesc, static_cast<hipTextureObject_t>(0)),
+                    hipErrorInvalidValue);
   }
-  
+
 #else
   HipTest::HIP_SKIP_TEST("Skipping on NVIDIA platform");
 #endif
-  
 }
+
+/* hipDestroyTextureObject test */
 
 TEST_CASE("Unit_hipDestroyTextureObject_positive") {
   CHECK_IMAGE_SUPPORT
 
   TextureObjectTestWrapper texObjWrapper(false, true);
   REQUIRE(hipDestroyTextureObject(texObjWrapper.mTextureObject) == hipSuccess);
-
 }
